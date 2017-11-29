320fd955c3c544d110ad0f9f7b4eea7c6f170eee
Guilherme Porto <guilherme.porto@lambda3.com.br>
2017-11-28 22:46:37 -0200

    Adicionando projeto de testes de unidade

---
+++ tests/RangeTest.cs
@@ -0,0 +1,19 @@
+using api.Controllers;
+using System.Linq;
+using Xunit;
+
+namespace tests
+{
+    public class RangeTests
+    {
+        [Fact]
+        public void CountShouldControlNumberOfResults()
+        {
+            var range = new Range { Count = 3 };
+
+            var generated = range.Of(() => "");
+
+            Assert.Equal(3, generated.Count());
+        }
+    }
+}
